d9615d1bc910b38efe02c8d06b48c6d634fb8397
halowens <halowens@pal-nat186-37-12.itap.purdue.edu>
2019-03-02 12:38:45 -0500

    Now they adhere to code standard. Probably...

--- Inc/maxsensor.h
+++ Inc/maxsensor.h
@@ -12,10 +12,10 @@
 
 
 typedef struct {
-	uint8_t pin; // Pin on the MAX Chip
-	MAX11615* p_max; //When were using both chips this can define which one is being used
-	uint16_t value; // holds the value of sensor is updated by calling the function below
-	uint8_t (*read)(sensor_t * p_self); //Should be called to get sensor value
+  uint8_t pin; // Pin on the MAX Chip
+  MAX11615* p_max; //When were using both chips this can define which one is being used
+  uint16_t value; // holds the value of sensor is updated by calling the function below
+  uint8_t (*read)(sensor_t * p_self); //Should be called to get sensor value
 }sensor_t;
 
 uint8_t maxsensor_Inlineflow_Read(sensor_t*);
